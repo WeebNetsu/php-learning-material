942d40f54044587b6d1b72737b84f82eba6ec242
WeebNetsu <stephenvdw7777@gmail.com>
2023-06-18 20:58:54 +0200

    Added more content to if statements

--- 6. If statement/conditions.php
+++ 6. If statement/conditions.php
@@ -11,6 +11,7 @@
        >=
        <=
        and
+       &&
        or
        ||
        xor
@@ -33,6 +34,17 @@ echo true == 1 ? "yes" : "no"; // yes
 echo true == false ? "yes" : "no"; // no
 echo true == 0 ? "yes" : "no"; // no
 echo true == [] ? "yes" : "no"; // no
+// === is more strict than ==, == will accept different
+// data types (like a number and a string) and compare
+// them, whilst === will check if both the data type
+// and the value are the same
+echo true === 1 ? "yes" : "no"; // no
+echo 1 === 1 ? "yes" : "no"; // yes
+echo 1 != "1" ? "yes" : "no"; // no
+// <> is the same as !=
+echo 1 <> "1" ? "yes" : "no"; // no
+// same concept as ===, this also compares data types
+echo 1 !== "1" ? "yes" : "no"; // yes
 
 // If statements stays the same as in most languages
 $num = 24;
@@ -45,12 +57,20 @@ if ($num > 25) {
     echo "It is medium";
 }
 
+echo 1 >= 1 ? "yes" : "no"; // yes
+echo 1 >= 2 ? "yes" : "no"; // no
+echo 1 >= 0 ? "yes" : "no"; // yes
+
+echo 1 <= 1 ? "yes" : "no"; // yes
+echo 1 <= 2 ? "yes" : "no"; // yes
+echo 1 <= 0 ? "yes" : "no"; // no
+
 //and
 if (2 > 3 and 3 > 4) {
     # code...
 }
 
-#is the same as this:
+#is the same as this `and`
 if (2 > 3 && 3 > 4) {
     # code...
 }
@@ -60,7 +80,7 @@ if (2 > 3 or 3 > 2) {
     # code...
 }
 
-//is the same as
+//is the same as `or`
 if (2 > 3 || 3 > 2) {
     # code...
 }
