3870503b19e39cea7da960df939e685fbaef42a2
WeebNetsu <stephenvdw7777@gmail.com>
2023-06-19 12:46:38 +0200

    Cleaned up working with files

--- 23. Writing to a file/write-to-file.php
+++ 23. Writing to a file/write-to-file.php
@@ -1,30 +1,31 @@
 <?php
-	//NOTE: The browser may not have the privelages to write to the file if using apache
-	//chmod 777 fileName.txt in the terminal fixes it
+// NOTE: The browser may not have the privileges to write to the file if using apache
+// chmod 777 fileName.txt in the terminal is a quick and dirty fix to it on Linux
 
-	// fopen() - creates or opens file
+/*
+    Types of commands:
+    r: Opens file to read only
+    w: Opens file to write only AND erases the content of the file OR creates a new file
+    a: Opens file to write only (appends the new text to the end of the file)
+    x: Create new file to write only
+    r+: Opens file to read/write
+    w+: Opens file to read/write AND erases the content of the file OR creates a new file
+    a+: Opens file to read/write and/or creates new file
+    x+: Creates new file to read/write
+*/
 
-	/*
-		Types of commands:
-		r: Opens file to read only
-		w: Opens file to write only AND erases the content of the file OR creates a new file
-		a: Opens file to write only (appends the new text to the end of the file)
-		x: Create new file to write only
-		r+: Opens file to read/write
-		w+: Opens file to read/write AND erases the content of the file OR creates a new file
-		a+: Opens file to read/write and/or creates new file
-		x+: Creates new file to read/write
-	*/
+// fopen() - creates or opens file
+// in the below case, open a file in write mode
+$myfile = fopen("test.txt", "w");
 
-	$myfile = fopen("test.txt", "w");
+// fwrite() - writes to a file
+$text = "My name is Tim\n";
+fwrite($myfile, $text);
 
-	// fwrite() - writes to a file
-	$text = "My name is Tim\n"; //\n creates new line
-	fwrite($myfile, $text);
+// write another line of text to the file
+$text = "My name is John";
+fwrite($myfile, $text);
 
-	$text = "My name is John";
-	fwrite($myfile, $text);
-
-	// Closes the file
-	fclose($myfile)
+// Closes the file after using it
+fclose($myfile);
 ?>
--- 24. Read from a file/readfile.php
+++ 24. Read from a file/readfile.php
@@ -1,16 +1,17 @@
 <?php
-	//file() - reads the entire file and puts it in an array
-	//each line has own index in array
-	$read = file('test.txt');
-	$count = count($read); //returns length of the array
+// file() - reads the entire file and puts it in an array
+// you can also use fopen in read mode
+$read = file('test.txt');
+// returns length of the array, or in this case how many lines the file had
+$count = count($read);
 
-	$i = 1;
-	foreach($read as $line){
-		echo "$line";
+$i = 1;
+foreach ($read as $line) {
+    echo "$line";
 
-		if($i < $count){
-			echo ", ";
-		}
-			$i++;
-	}
+    if ($i < $count) {
+        echo ", ";
+    }
+    $i++;
+}
 ?>
